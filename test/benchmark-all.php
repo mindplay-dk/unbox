c96e9215c02a44338b2051bb73d36ef634b229f7
Rasmus Schultz <rasc@fynskemedier.dk>
2015-08-19 17:13:42 +0200

    self-benchmark

---
+++ test/benchmark-all.php
@@ -0,0 +1,81 @@
+<?php
+
+// rough benchmark comparing the time required to bootstrap and
+// resolve the dependencies in a simple test-case.
+
+use mindplay\benchpress\Benchmark;
+
+require __DIR__ . '/header.php';
+
+$bench = new Benchmark();
+
+$unbox_configuration = function () {
+    $container = require __DIR__ . '/bootstrap-unbox.php';
+};
+
+$phpdi_configuration = function () {
+    $container = require __DIR__ . '/bootstrap-php-di.php';
+};
+
+$pimple_configuration = function () {
+    $container = require __DIR__ . '/bootstrap-pimple.php';
+};
+
+$bench->add(
+    'unbox: configuration',
+    $unbox_configuration
+);
+
+$bench->add(
+    'php-di: configuration',
+    $phpdi_configuration
+);
+
+$bench->add(
+    'pimple: configuration',
+    $pimple_configuration
+);
+
+$bench->run();
+
+foreach (array(1,3,5,10) as $num) {
+    $bench = new Benchmark();
+
+    $bench->add(
+        "unbox: {$num} repeated resolutions",
+        function () use ($num) {
+            $container = require __DIR__ . '/bootstrap-unbox.php';
+
+            for ($i = 0; $i < $num; $i++) {
+                $cache = $container->get(UserRepository::class);
+            }
+        },
+        $unbox_configuration
+    );
+
+    $bench->add(
+        "php-di: {$num} repeated resolutions",
+        function () use ($num) {
+            $container = require __DIR__ . '/bootstrap-php-di.php';
+
+            for ($i = 0; $i < $num; $i++) {
+                $cache = $container->get(UserRepository::class);
+            }
+        },
+        $phpdi_configuration
+    );
+
+    $bench->add(
+        "pimple: {$num} repeated resolutions",
+        function () use ($num) {
+            $container = require __DIR__ . '/bootstrap-pimple.php';
+
+            for ($i = 0; $i < $num; $i++) {
+                $cache = $container[UserRepository::class];
+            }
+        },
+        $pimple_configuration
+    );
+
+    $bench->run();
+}
--- test/benchmark.php
+++ test/benchmark.php
@@ -1,8 +1,5 @@
 <?php
 
-// rough benchmark comparing the time required to bootstrap and
-// resolve the dependencies in a simple test-case.
-
 use mindplay\benchpress\Benchmark;
 
 require __DIR__ . '/header.php';
@@ -13,36 +10,14 @@ $unbox_configuration = function () {
     $container = require __DIR__ . '/bootstrap-unbox.php';
 };
 
-$phpdi_configuration = function () {
-    $container = require __DIR__ . '/bootstrap-php-di.php';
-};
-
-$pimple_configuration = function () {
-    $container = require __DIR__ . '/bootstrap-pimple.php';
-};
-
 $bench->add(
-    'unbox: configuration',
+    'configuration',
     $unbox_configuration
 );
 
-$bench->add(
-    'php-di: configuration',
-    $phpdi_configuration
-);
-
-$bench->add(
-    'pimple: configuration',
-    $pimple_configuration
-);
-
-$bench->run();
-
-foreach (array(1,3,5,10) as $num) {
-    $bench = new Benchmark();
-
+foreach (array(1,10) as $num) {
     $bench->add(
-        "unbox: {$num} repeated resolutions",
+        "{$num} repeated resolutions",
         function () use ($num) {
             $container = require __DIR__ . '/bootstrap-unbox.php';
 
@@ -52,30 +27,6 @@ foreach (array(1,3,5,10) as $num) {
         },
         $unbox_configuration
     );
-
-    $bench->add(
-        "php-di: {$num} repeated resolutions",
-        function () use ($num) {
-            $container = require __DIR__ . '/bootstrap-php-di.php';
-
-            for ($i = 0; $i < $num; $i++) {
-                $cache = $container->get(UserRepository::class);
-            }
-        },
-        $phpdi_configuration
-    );
-
-    $bench->add(
-        "pimple: {$num} repeated resolutions",
-        function () use ($num) {
-            $container = require __DIR__ . '/bootstrap-pimple.php';
-
-            for ($i = 0; $i < $num; $i++) {
-                $cache = $container[UserRepository::class];
-            }
-        },
-        $pimple_configuration
-    );
-
-    $bench->run();
 }
+
+$bench->run();
